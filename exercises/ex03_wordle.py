b7ccfbfe85d6f8097fd2f2c2fef19e5c34f5cbc2
Ashley Neall <aneall@unc.edu>
2022-09-14 23:40:47 -0400

    EX03 Attempt 1

---
+++ exercises/ex03_wordle.py
@@ -0,0 +1,67 @@
+"""My EX03, where I create a structured wordle game."""
+__author__ = "730604478"
+
+def contains_char(str_to_search: str, guess_char: str) -> bool:
+    """Function for finding a character within another string."""
+    assert len(guess_char) == 1
+    str_index: int = 0
+    while str_index < len(str_to_search):
+        if str_to_search[str_index] == guess_char:
+            return True
+        else: 
+            str_index += 1
+    return False
+
+
+def emojified(guess: str, secret: str) -> str:
+    """Function for codifying str of emoji boxes"""
+    assert len(guess) == len(secret)
+    WHITE_BOX: str = "\U00002B1C" 
+    GREEN_BOX: str = "\U0001F7E9"
+    YELLOW_BOX: str = "\U0001F7E8"
+
+    codify_index: int = 0
+    codify_str: str = ""
+    while codify_index < len(guess):
+        if guess[codify_index] == secret[codify_index]:
+            codify_str += GREEN_BOX
+        else:
+            if contains_char(secret, guess[codify_index]):
+                codify_str += YELLOW_BOX
+            else:
+                codify_str += WHITE_BOX
+        codify_index += 1
+    return codify_str
+
+
+def input_guess(expected_length: int) -> str:
+    """Function for????"""
+    user_guess: str = input(f"Enter your {expected_length} character word: ")
+    guess_index: int = 0   # here we initialize the index of the user's guessed word to 0
+    while len(user_guess) != expected_length:
+        user_guess = input(f"That wasn't {expected_length} chars! Try again: ")
+    return user_guess
+
+def main() -> None:
+    """The entrypoint of the program and main game loop."""
+    secret_word: str = "codes"
+    input_str: str = ""
+    emojified_str: str = ""
+    N: int = 1
+    win: bool = False
+    while N < 7 and win == False:
+        print(f"=== Turn {N}/6 ===")
+        input_str = input_guess(len(secret_word))
+        emojified_str = emojified(input_str, secret_word)
+        print(emojified_str)
+        if input_str == secret_word:
+            win = True
+        else:
+            N += 1
+    if win:
+        print(f"You won in {N}/6 turns!")
+    else:
+        print("X/6 - Sorry, try again tomorrow!")
+
+if __name__ == "__main__":
+    main()
---
+++ lessons/love_functions.py
@@ -0,0 +1,16 @@
+"""Some tender, loving functions."""
+
+def love(subject: str) -> str:
+    """Given a subject as a parameter, returns a loving string."""
+    return (f"I love you {subject}!")
+
+def spread_love(to: str, n: int) -> str:
+    """Generates a str repeating a loving message n times."""
+    love_note: str = ""
+    count: int = 0
+    while count < n:
+        love_note += love(to) + "\n"
+        count += 1
+    return love_note
+
+
