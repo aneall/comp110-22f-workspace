6afd9548f274076de282d1dd0d2437b02e3eb244
Ashley Neall <aneall@unc.edu>
2022-09-26 23:29:13 -0400

    final ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -3,6 +3,7 @@ __author__ = "730604478"
 
 
 def only_evens(x: list[int]) -> list:
+    """The only_evens function runs through a single given list of integers and returns a new list containing only the even values in the list, in their appearing order."""
     i: int = 0
     evens_list: list = []
     while i < len(x):
@@ -12,8 +13,8 @@ def only_evens(x: list[int]) -> list:
     return evens_list
 
 
-
 def concat(x: list[int], y: list[int]) -> list:
+    """The concat function runs through two lists; the first list is run through and each value at every index is appended to a new list in their appearing order, followed by all of the values at each index in the second given list; a new list is created from this and returned, showing all values from both lists respectively."""
     i: int = 0
     new_list: list = []
     while i < len(x):
@@ -26,9 +27,8 @@ def concat(x: list[int], y: list[int]) -> list:
     return new_list
 
 
-
-
 def sub(x: list[int], start: int, end: int) -> list:
+    """The sub function runs through a single given list of integers and uses given 'start' and 'end' integer values to figure out what part of a list (as in, through what range of indices in the given 'x' list) should be returned in a new list."""
     new_list: list = []
     if len(x) == 0 or start > len(x) or end <= 0:
         return new_list
