bbadd7b3be05b1e844f4fc1a7a0c4378b3f9259f
Ashley Neall <aneall@unc.edu>
2022-10-06 00:13:05 -0400

    ex06 6

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -21,7 +21,7 @@ def end_game() -> None:
 
 
 def bonus_round() -> None:
-    """The """
+    """The bonus_round function prompts the user to answer an additional guessing question to possibly receive an extra 10 points. As menioned in the main function, thsi will only happen once user's current points is at 0."""
     guess: int = input("Pick a number between 1 and 10 to earn an extra 10 points: ")
     random_int: int = random.randint(1, 10)
     if guess == random_int:
@@ -33,6 +33,7 @@ def bonus_round() -> None:
 
 
 def check_guess(x: int) -> int:
+    """The check_guess function uses a random of integer of either 0 or 1 to represent heads or tails. When the user's guess of 0 or 1 is passed to this function, it will return 2 (which represents correct guess) or 3 (which represents false guess)."""
     # takes a random number and converts such to heads or tails
     # 0 is heads and 1 is tails
     random_int: int = random.randint(0, 1)
@@ -44,7 +45,8 @@ def check_guess(x: int) -> int:
 
 
 def big_bonus(x: int) -> int:
-    if x == 1:
+    """The big_bonus function prompts the user to answer an addiitonal guessing question when current points is equal to 3. If answered correctly, the user's points increases by 20."""
+    if x == 3:
         color_guess: str = input("What is my favorite color? ")
         if color_guess == "blue":
             print(f"Wow, {player}, You are a mind reader! You just earned 20 additional points!")
@@ -54,20 +56,18 @@ def big_bonus(x: int) -> int:
     return x
 
 
-
-
-
 def main() -> None:
+    """The main function handles the game loop and makes the process of acccessing branches of the game and calling needed functions more efficient."""
     greet()
     global points
     user_guess: int = 0
     heads_or_tails: int = 0
-    while user_guess < 2:
-        points = big_bonus(points)
+    while user_guess < 2:  # this is where the game loop begins, in which as long as user's guess is 0 (heads) or 1 (tails), the loop will run.
+        points = big_bonus(points)  # here we call the 20-point bonus function and will add 20 to points' value.
         if points == 1:
-            bonus_round()
+            bonus_round()  # here we call the 10-point bonus function and will add 10 to points' value when points currently equals 1 before entering such function.
         user_guess = int(input("Enter 0 for heads, 1 for tails, or 2 to end the coin flip game. "))
-        heads_or_tails = check_guess(user_guess)
+        heads_or_tails = check_guess(user_guess)  # on the following lines we use a variable (heads_or_tails) to keep track of if the user guessed correctly (2) or incorrectly (3) so that the correct output message can be sent and points are correctly updated.
         if heads_or_tails == 2:
             points += 1
             print("Congratulations, you guessed correctly so you gained 1 point!")
