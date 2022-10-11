2338b009db86d8421d2cc812a1c98e9b927f8b92
Ashley Neall <aneall@unc.edu>
2022-10-10 23:56:47 -0400

    ex07 4

--- exercises/ex07/dictionary_test.py
+++ exercises/ex07/dictionary_test.py
@@ -12,9 +12,11 @@ def test_invert_all_same() -> None:
         my_dictionary = {"a": "hello", "b": "hello", "c": "hello"}
         invert(my_dictionary)
 
+
 def test_invert_three_elements() -> None:
     """A test to see that if three pairs are entered as our argument, it will result in three inverted pairs."""
     assert invert({"a": "1", "b": "2", "c": "3"}) == {"1": "a", "2": "b", "3": "c"}
+
 
 def test_invert_one_elements() -> None:
     """A test to see that if one pair is entered as our argument, it will result in one inverted pair."""
@@ -25,9 +27,11 @@ def test_only_one_favorite_color() -> None:
     """A test to see if when only one color is in the argument, it returns that color as the favorite."""
     assert favorite_color({"Ashley": "Pink"}) == "Pink"
 
+
 def test_two_favorite_colors() -> None:
     """A test to see if when two diffreent colors are in the argument, it returns the color mentioned first as the favorite."""
     assert favorite_color({"Sally": "Blue", "Harry": "Green"}) == "Blue"
+
 
 def test_three_favorite_colors() -> None:
     """A test to see if when three colors, one of which is mentioned twice, are in the argument, it returns the color mentioned twice as the favorite."""
@@ -37,10 +41,12 @@ def test_three_favorite_colors() -> None:
 def test_count_three_elements() -> None:
     """A test to see if when three str,one of which is mentioned twice, are in the argument, both are correctly counted for instances they are mentioned and returned as a dictionary."""
     assert count(["a", "b", "a"]) == {"a": 2, "b": 1}
-    
+
+
 def test_count_empty_list() -> None:
     """A test to see if when an empty list is the argument, an empty dictionary is returned."""
     assert count([]) == {}
+  
     
 def test_count_three_same() -> None:
     """A test to see if when three of the same str are given as the argument, a dictionary counting the str designates the value as 3 to signify that is was mentioned three times."""
